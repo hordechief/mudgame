5255739339b48f5678243506449c6c4de0c9c683
navicester <navicester@qq.com>
2018-09-16 23:33:30 +0800

    Server2 with SocketSet

--- server/server.cpp
+++ server/server.cpp
@@ -53,173 +53,81 @@
 using namespace std;            // use the std namespace
 
 #include "SocketLib/SocketLibSocket.h"
+#include "SocketLib/SocketSet.h"
 using namespace SocketLib;
 
 #include "SocketLib/SocketLibSystem.h"
 
 int main()
 {	
-	ListeningSocket _lsock;
-	_lsock.Listen(4000);
-	DataSocket _dsock;
-	_dsock = _lsock.Accept();
-	
-	char _buffer[128] = "Hello There!";
-//    _dsock.Send( buffer, strlen( buffer ));
-    _dsock.Receive(_buffer, 128);
-    cout << "Data received:" << endl;
-    cout << _buffer << endl;    
-
-	return 0;
-
     int err;                    // for getting errors
-    int lsock;                  // listening socket
-    vector<int> socketlist;     // list of sockets
-
-    // start the socket library
-    StartSocketLib;
-
-    // BEGIN CODE BLOCK 2.3 - Create a Listening Socket on port 4000
-    // create a socket
-    lsock = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
-
-    // check if socket was created
-    if( lsock == -1 )
-    {
-        cout << "Socket creation error!" << endl;
-        return 0;
-    }
-    cout << "Socket created!" << endl;
-
-    // create a sockaddr_in for binding, listening on port 4000
-    struct sockaddr_in socketaddress;
-    socklen_t sa_size = sizeof( struct sockaddr_in );
-    socketaddress.sin_family = AF_INET;
-    socketaddress.sin_port = htons( 4000 );
-    socketaddress.sin_addr.s_addr = htonl( INADDR_ANY );
-    memset( &(socketaddress.sin_zero), 0, 8 );
-
-    // bind the socket
-    err = bind( lsock, (struct sockaddr*)&socketaddress, sa_size );
-
-    if( err == -1 )
-    {
-        cout << "Socket binding error!" << endl;
-        return 0;
-    }
-    cout << "Socket bound!" << endl;
-
-
-    // listen on the socket
-    err = listen( lsock, 16 );
-
-    if( err == -1 )
-    {
-        cout << "Socket listening error!" << endl;
-        return 0;
-    }
-    cout << "Socket listening, waiting for connection..." << endl;
-    // END CODE BLOCK 2.3 - Create a Listening Socket on port 4000
-
-
-    fd_set rset;
-    int i;
-
-    struct timeval zerotime;
-    zerotime.tv_usec = 0;
-    zerotime.tv_sec = 0;
-
-    char buffer[128];           // used for getting messages
+    
+	ListeningSocket lsock;
+	lsock.Listen(4000);
+	DataSocket dsock;
+	
+	char buffer[128] = "Hello There!"; 
+    
     bool done = false;          // used for quitting
-    vector<int>::iterator itr;
-
-    // now begin the main loop.
-    while( !done )
-    {
-        // clear the set
-        FD_ZERO( &rset );
-
-        // add the listening socket
-        FD_SET( lsock, &rset );
-
+    vector<DataSocket>::iterator itr;
+    vector<DataSocket> socketlist;     // list of sockets
+    
+    while( !done ){
+    	
+        SocketSet sset;
+		    	
+    	// add listen sockets
+    	sset.AddSocket( lsock );
+    	
         // add all of the data sockets
         for( itr = socketlist.begin(); itr != socketlist.end(); itr++ )
         {
-            FD_SET( *itr, &rset );
+            sset.AddSocket( *itr );
+//            cout << "add all of the data sockets " << itr->GetSock() << "..." << endl;
         }
         
-        // find out if there is any activity on any of the sockets.
-        i = select( 0x7FFFFFFF, &rset, NULL, NULL, &zerotime );
-
-        if( i > 0 )
-        {
-            if( FD_ISSET( lsock, &rset ) )
-            {
-                // incomming connection
-                cout << "Incomming connection..." << endl;
-                int dsock = accept( lsock, 
-                                    (struct sockaddr*)&socketaddress, 
-                                    &sa_size );
-                if( dsock == -1 )
-                {
-                    cout << "Socket accepting error!" << endl;
-                    return 0;
-                }
-                cout << "Socket " << dsock << " accepted." << endl;
-                
-                // add the socket to the list
-                socketlist.push_back( dsock );
-            }
-
+        int active = sset.Poll( 1000 );
+        if(active)
+		{
+        	if(sset.HasActivity(lsock))
+			{
+			    DataSocket dsock;
+			    dsock = lsock.Accept();
+			    socketlist.push_back( dsock );
+            }		
+		
             // loop through each socket and see if it has any activity
             for( itr = socketlist.begin(); itr != socketlist.end(); itr++ )
             {
-                if( FD_ISSET( *itr, &rset ) )
-                {
-                    // incomming data
-                    cout << "receiving data from socket ";
-                    cout << *itr << "..." << endl;
-                    err = recv( *itr, buffer, 128, 0 );
+            	if(sset.HasActivity(*itr))
+            	{
+	            	// incomming data
+	                cout << "receiving data from socket " << itr->GetSock() << "..." << endl;
 
-                    if( err == -1 )
-                    {
-                        cout << "Socket receiving error!" << endl;
-                        return 0;
-                    }
-                    if( err == 0 )
-                    {
-                        cout << "Socket " << *itr << " closed" << endl;
-                        shutdown( *itr, 2 );
-                        CloseSocket( *itr );
-                        socketlist.erase( itr );
-                        itr--;
-                    }
-                    else
-                    {
-
-                        cout << "Data: " << buffer << endl;
-
-                        // if the message was "servquit", then quit the server.
-                        if( strcmp( buffer, "servquit" ) == 0 )
-                            done = true;
-                    }
+	                try
+					{					
+		                err = itr->Receive(buffer, 128);
+		                if(err){
+		                	cout << "Data: " << buffer << endl;
+						}
+		            }
+		            catch(Exception& e)
+					{				
+		            	cout << "Exception Error: " << err << endl;
+		            	socketlist.erase( itr );
+		            	itr--;
+		            }
+	
+	                // if the message was "servquit", then quit the server.
+	                if( strcmp( buffer, "servquit" ) == 0 )
+	                    done = true;
                 }
             }
-        }
+		}	
+	}
 
-    }
+	return 0;
 
-
-    shutdown( lsock, 2 );
-    CloseSocket( lsock );
-
-    for( i = 0; i < socketlist.size(); i++ )
-    {
-        shutdown( socketlist[i], 2 );
-        CloseSocket( socketlist[i] );
-    }
-
-    CloseSocketLib;
 }
 
 
